3634ef44d9c3bf82a744517b4d5526795ceeca6d
INVEKOR\Administrator <mert1723@gmail.com>
2022-07-19 17:36:04 +0300

    first commit

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wcol-md-6">
+    <h3>Child Component 1</h3>
+    <p>Ben User.vue isimli Parent Component'in içerisindeki bir Child componentim</p>
+    <p> Kullanıcı Adı : {{ name }}</p>
+    <p> Kullanıcı Adı : {{ switchName() }}</p>
+    <p> Kullanıcı Yaşı : {{ age }}</p>
+    <button @click="sendToParent"> Veriyi Parent'a Gönder</button>
+  </div>
+</template>
+<script>
+
+  export default {
+    props : {
+      name : {
+        type : String,
+        default : "videosinif.com"
+      },
+      age : String
+    },
+    methods : {
+      switchName() {
+        return this.name.split("").reverse().join("");
+      },
+      sendToParent(){
+        this.$emit("data", "randomdata");
+      }
+    }
+  }
+</script>
+<style scoped>
+  div {
+    background-color: lightcoral;
+    padding: 20px;
+    border: 1px solid #666;
+    display: inline-block;
+  }
+</style>
